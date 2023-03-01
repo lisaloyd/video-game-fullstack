9b5c8e7481ff5aa61702797522497531c1f3eb9d
unknown <greateducation123@yahoo.com>
2023-03-01 14:32:22 -0600

    Created functions inside of DisplayPlatforStats Component

--- video-game-frontend/src/Components/DisplayPlatformStats/DisplayPlatformStats.jsx
+++ video-game-frontend/src/Components/DisplayPlatformStats/DisplayPlatformStats.jsx
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'react-google-charts';
+import React, { useState, useEffect } from "react";
+import { Chart } from "react-google-charts";
 
-const DisplayPlatformStats = ({videoGames}) => {
+const DisplayPlatformStats = ({ videoGames }) => {
+  function generateDataFormChart() {
+    //Filter the video game by year
+    let filteredGames = videoGames.filter((game) => game.year > 2013);
 
-    function generateDataFormChart() {
+    let platforms = filteredGames.map((game) => {
+      return game.platform;
+    });
+
+    //uniq = [...new Set(array)];
+
+    let distinctPlatforms = [...new Set(platforms)];
+
+    let platformArrays = distinctPlatforms.map((platform) => {
+      
+   
+     let allGamesForPlatform = filteredGames.filter(game => game.platform == platform);
+
+     //now we need to loop through allGamesForPlatform and sum each games global sales
+
+    // Once we have the sum of all of those games global sales inserit it where '10' is
+     return [platform, 10, "silver"]
+    }); 
+
+    const data = [
+                ["Platform", "Sales", { role: "style" }], 
+                ...platformArrays
+            ];
         
-         //Filter the video game by year        
-         let filteredGames = videoGames.filter(game => game.year > 2013);
-         
+            console.log("Data", data)
+    
+    return data;
+  }
 
-         let platforms = filteredGames.map(game => {
-            return game.platform
-         });
-         
-
-         //uniq = [...new Set(array)];
-
-         let distinctPlatforms = [...new Set(platforms)];
-         console.log('distinct platforms', distinctPlatforms)
-
-         const data = [
-            ["Platform", "Sales", { role: "style" }],
-            ["PS3", 8.94, "silver"], 
-            ["Silver", 10.49, "silver"], 
-            ["Gold", 19.3, "silver"],
-            ["Platinum", 21.45, "color: silver"], 
-          ];
-
-          return data;
-    }
-
-
-    return(
-        <div>
-            <h1>Platform By Global Sales in Millions</h1>
-            <Chart chartType="ColumnChart" width="100%" height="400px" data={generateDataFormChart()} />
-
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Platform By Global Sales in Millions</h1>
+      <Chart
+        chartType="ColumnChart"
+        width="100%"
+        height="400px"
+        data={generateDataFormChart()}
+      />
+    </div>
+  );
+};
 
 export default DisplayPlatformStats;
