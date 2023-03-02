dcbdbb368e235bcee8fe6fd10f71a04902729e32
Nada Atieh <nada.s.atieh@gmail.com>
2023-03-02 14:22:35 -0600

    Removed code and imports in Navbar component + Modified the App component to include the Navbar import and HTML as well as imported the Container

--- video-game-frontend/src/App.jsx
+++ video-game-frontend/src/App.jsx
@@ -1,32 +1,34 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import DisplayPlatformStats from './Components/DisplayPlatformStats/DisplayPlatformStats';
-import VideoGameTable from './Components/VideoGameTable/VideoGameTable';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import DisplayPlatformStats from "./Components/DisplayPlatformStats/DisplayPlatformStats";
+import VideoGameTable from "./Components/VideoGameTable/VideoGameTable";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/esm/Container.js';
 
 function App() {
+  const [videoGames, setVideoGames] = useState([]);
 
-const [videoGames, setVideoGames] = useState([]);
+  useEffect(() => {
+    getVideoGames();
+  }, []);
 
-useEffect (() => {
-  getVideoGames();
-}, []);
-
-async function getVideoGames() {
- 
-  
-    const response = await axios.get('http://localhost:8080/all/');
-    setVideoGames(response.data); 
-
+  async function getVideoGames() {
+    const response = await axios.get("http://localhost:8080/all/");
+    setVideoGames(response.data);
   }
-
 
   return (
     <div>
-        <DisplayPlatformStats videoGames={videoGames}/>
-        <VideoGameTable videoGames={videoGames}/>
-        
+      <Navbar bg='light' fixed='top'>
+        <Container>
+          <Navbar.Brand>GameInfo</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div>
+        <DisplayPlatformStats videoGames={videoGames} />
+        <VideoGameTable videoGames={videoGames} />
+      </div>
     </div>
-
   );
 }
 
